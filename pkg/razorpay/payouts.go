61cea1d43371fa6776876733da913529cd0e68d7
razorng <goyal.naman@razorpay.com>
2025-05-07 14:25:53 +0530

    chore : added description and convention

--- pkg/razorpay/payouts.go
+++ pkg/razorpay/payouts.go
@@ -19,8 +19,7 @@ func FetchPayout(
 		mcpgo.WithString(
 			"payout_id",
 			mcpgo.Description(
-				"The unique identifier of the payout to fetch. Format: pout_ "+
-					"followed by alphanumeric characters (e.g., pout_qr2726363738)",
+				"The unique identifier of the payout. For example, 'pout_00000000000001'",
 			),
 			mcpgo.Required(),
 		),
@@ -30,17 +29,17 @@ func FetchPayout(
 		ctx context.Context,
 		r mcpgo.CallToolRequest,
 	) (*mcpgo.ToolResult, error) {
-		payload := make(map[string]interface{})
+		FetchPayoutOptions := make(map[string]interface{})
 
 		validator := NewValidator(&r).
-			ValidateAndAddRequiredString(payload, "payout_id")
+			ValidateAndAddRequiredString(FetchPayoutOptions, "payout_id")
 
 		if result, err := validator.HandleErrorsIfAny(); result != nil {
 			return result, err
 		}
 
 		payout, err := client.Payout.Fetch(
-			payload["payout_id"].(string),
+			FetchPayoutOptions["payout_id"].(string),
 			nil,
 			nil,
 		)
@@ -68,17 +67,21 @@ func FetchAllPayouts(
 	parameters := []mcpgo.ToolParameter{
 		mcpgo.WithString(
 			"account_number",
-			mcpgo.Description("The account number to fetch payouts for"),
+			mcpgo.Description("The account from which the payouts were done."+
+				"For example, 7878780080316316"),
 			mcpgo.Required(),
 		),
 		mcpgo.WithNumber(
 			"count",
-			mcpgo.Description("Number of payouts to fetch (default: 10)"),
+			mcpgo.Description("Number of payouts to be fetched. Default value is 10."+
+				"Maximum value is 100. This can be used for pagination,"+
+				"in combination with the skip parameter"),
 			mcpgo.Min(1),
 		),
 		mcpgo.WithNumber(
 			"skip",
-			mcpgo.Description("Number of payouts to skip (for pagination)"),
+			mcpgo.Description("Numbers of payouts to be skipped. Default value is 0."+
+				"This can be used for pagination, in combination with count"),
 			mcpgo.Min(0),
 		),
 	}
@@ -87,17 +90,17 @@ func FetchAllPayouts(
 		ctx context.Context,
 		r mcpgo.CallToolRequest,
 	) (*mcpgo.ToolResult, error) {
-		payload := make(map[string]interface{})
+		FetchAllPayoutsOptions := make(map[string]interface{})
 
 		validator := NewValidator(&r).
-			ValidateAndAddRequiredString(payload, "account_number").
-			ValidateAndAddPagination(payload)
+			ValidateAndAddRequiredString(FetchAllPayoutsOptions, "account_number").
+			ValidateAndAddPagination(FetchAllPayoutsOptions)
 
 		if result, err := validator.HandleErrorsIfAny(); result != nil {
 			return result, err
 		}
 
-		payout, err := client.Payout.All(payload, nil)
+		payout, err := client.Payout.All(FetchAllPayoutsOptions, nil)
 		if err != nil {
 			return mcpgo.NewToolResultError(
 				fmt.Sprintf("fetching payouts failed: %s", err.Error())), nil
